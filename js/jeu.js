d7953858f5faf3f765f223a2b7ce5b0adbcc2a76
Fanilo Harilalaina Randrianjafy <frandria@bbdo.fr>
2017-04-21 11:38:08 +0400

    updated color brick

--- js/jeu.js
+++ js/jeu.js
@@ -11,22 +11,39 @@ class Jeu {
     this.collision;
     this.score;
     this.audio = {};
+    this.listeCouleur = [];
   }
 
+  creerCouleur(){
+     
+      
+      for(let nombre = 0; nombre<7; nombre++){
+         let R = Math.round(255 * Math.random());
+      let G = Math.round(255 * Math.random());
+      let B = Math.round(255 * Math.random());
+        let couleur = "rgb(" + R + "," + G + "," + B +")";
+        //console.log(couleur);
+        this.listeCouleur.push("rgb(" + R + "," + G + "," + B +")");
+        
+      }
+   
+  }
   creerBriques() {
 
-    for(let nombre = 1; nombre<7; nombre++){
-		for(let i = 0; i < 8; i++) {
-			let width = 67;
-			let height = 30;
-			let x = i * (width+7) +7;
-			let y = 30 + (height+7)*(nombre-1);
-			let couleur = "rgb(245,124,145)";
+    for(let nombre = 0; nombre<7; nombre++){
+		for(let i = 0; i < 7; i++) {
+			let width = 45;
+			let height = 20;
+			let x = i * (width+6) +6;
+			let y = 60 + (height+6)*(nombre-1);
+			
 			let vx = 0;
 	    	let vy = 0;
-			let listeBrique = new Brique(x, y, this.context, couleur, width, height);
+        
+			let listeBrique = new Brique(x, y, this.context, this.listeCouleur[nombre], width, height);
       listeBrique.draw();
 			this.briques.push(listeBrique);
+     
 		}
 	}
   }
@@ -81,7 +98,7 @@ class Jeu {
       this.context.translate(this.canvas.width / 2 - 70, this.canvas.height / 2);
       this.context.fillStyle = 'white';
       this.context.font = '30px Arial bold';
-      this.context.fillText('GAME OVER', 0, 0);
+      this.context.fillText('Vous avez perdu', 0, 0);
       this.context.restore();
       this.animation(time);
     }
@@ -95,6 +112,7 @@ class Jeu {
     this.canvas = document.querySelector(this.selecteur);
     this.context = this.canvas.getContext('2d');
     this.score = new Score(this.context, 1);
+    this.creerCouleur();
     this.creerPlateau();
     this.creerBriques();
     this.creerBalles();
@@ -108,38 +126,45 @@ class Jeu {
     ecouteur.ecouter();
     this.ecouteur = ecouteur;
     console.log(this);
+    
     requestAnimationFrame((time) => this.animation(time));
   }
 
   animation(time) {
-    this.fps.measureFPS(time);
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.briques.map(brique => {
-      brique.draw();
-    });
-    this.plateau.draw();
-    this.taquet.draw();
-    this.score.draw();
-    this.deplacerBalles();
-    if(this.increment === 200) {
-      let x = Math.random() * this.canvas.width;
-      let y = Math.floor(Math.random() * (this.plateau.y - (this.briques[this.briques.length - 1].y + 30) + 1)  + (this.briques[this.briques.length - 1].y + 30));
-      let rayon = 8;
-      let R = Math.round(255 * Math.random());
-      let G = Math.round(255 * Math.random());
-      let B = Math.round(255 * Math.random());
-      let couleur = "rgb(" + R + "," + G + "," + B +")";
-      let vx = 1 + Math.random() * 3;
-      let vy = 1 + Math.random() * 3;
-      const balle = new Balle(x, y, rayon, this.context, couleur, vx, vy);
-      /* On le mets dans le tableaux de balles */
-      this.balles.push(balle);
-      this.increment = 0;
-    };
-    // tester si la partie est terminer
-    this.partieTerminer();
-    this.increment++;
-    requestAnimationFrame((time) => this.animation(time));
+    console.log(this.briques.length);
+    if(this.briques.length != 0)
+    {
+      this.fps.measureFPS(time);
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.briques.map(brique => {
+        brique.draw();
+      });
+      this.plateau.draw();
+      this.taquet.draw();
+      this.score.draw();
+      this.deplacerBalles();
+      if(this.increment === 200) {
+        let x = Math.random() * this.canvas.width;
+        let y = Math.floor(Math.random() * (this.plateau.y - (this.briques[this.briques.length - 1].y + 30) + 1)  + (this.briques[this.briques.length - 1].y + 30));
+        let rayon = 8;
+        let R = Math.round(255 * Math.random());
+        let G = Math.round(255 * Math.random());
+        let B = Math.round(255 * Math.random());
+        let couleur = "rgb(" + R + "," + G + "," + B +")";
+        let vx = 1 + Math.random() * 3;
+        let vy = 1 + Math.random() * 3;
+        const balle = new Balle(x, y, rayon, this.context, couleur, vx, vy);
+        /* On le mets dans le tableaux de balles */
+        this.balles.push(balle);
+        this.increment = 0;
+      };
+      // tester si la partie est terminer
+      this.partieTerminer();
+      this.increment++;
+	
+      requestAnimationFrame((time) => this.animation(time));
+    }
+
   }
 
 }
